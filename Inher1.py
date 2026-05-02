2a868788dc44df17008e4bb9431ab9e11d912748
Viplavah112 <biplabpoudel112@gmail.com>
2026-05-02 15:24:16 +0545

    First commit

---
+++ Inher1.py
@@ -0,0 +1,25 @@
+class Person:
+    def __init__(self,fname,lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname(self):
+        print(self.firstname, self.lastname)
+
+
+x= Person("John","Doe")
+x.printname()       
+
+
+
+
+
+#creating child class
+
+class Student(Person):
+    pass
+
+
+
+x= Student("Mike","Olsen")
+x.printname()
---
+++ ex1.py
@@ -0,0 +1,7 @@
+class Person:
+    def person(self):
+        print("I am a person")
+
+
+p = Person()
+p.person()
